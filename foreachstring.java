b49ae7601890df5eb4eafedb5a9b37b1c6ae03db
sonu79934 <yogasritha8@gmail.com>
2024-12-10 11:13:55 +0530

    Add files via upload

---
+++ foreachstring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class foreachstring{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a[]={"Apple","Banana","Mango"};
+        for(String b:a)
+        System.out.print(b+",");
+    }
+}
