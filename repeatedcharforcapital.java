0e28dc10a52909dd682e3d8f66062e461e912a23
sonu79934 <yogasritha8@gmail.com>
2024-12-10 15:35:57 +0530

    Add files via upload

---
+++ repeatedcharforcapital.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class repeatedcharforcapital{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        //for capital letters
+        String a="UDAYANIEEE";
+        int c[]=new int[26];
+        for(int i=0;i<a.length();i++)
+        {
+            c[(int)(a.charAt(i))-65]++;
+        }
+        int max=0;
+        int index=0;
+        for(int i=0;i<26;i++)
+        {
+            if(c[i]>max){
+            max=c[i];
+            index=i;
+        }
+        }
+        System.out.println((char)(index +65));
+    }
+}
