57c2956fda28189bb7cd73c18d772f59b0f89221
sonu79934 <yogasritha8@gmail.com>
2024-12-10 10:58:49 +0530

    Add files via upload

---
+++ reversingastringusingnormalapproach.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class reversingastringusingnormalapproach{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        String res="";
+        for(int i=0;i<a.length();i++)
+        res=a.charAt(i)+res;
+        System.out.println(res);
+    }
+}
