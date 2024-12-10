8d0d50b40f83bcf2d097cf4e5fc8fbe908e8e82f
sonu79934 <yogasritha8@gmail.com>
2024-12-10 14:20:29 +0530

    Add files via upload

---
+++ camelcasestring.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class camelcasestring{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String res=" ";
+        for(int i=0;i<a.length();i++)
+        {
+            char s=a.charAt(i);
+            s^=32;
+            res+=s;
+        }
+        System.out.println(res);
+    }
+}
